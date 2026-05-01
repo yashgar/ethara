fe1ed7a7ad3fde799ad0987de662ab00d7a958e7
Yash Agarwal <117198025+yashgar@users.noreply.github.com>
2026-05-02 02:47:20 +0530

    Update db.py

--- backend/db/db.py
+++ backend/db/db.py
@@ -1,10 +1,8 @@
 from pymongo import MongoClient
 import os
-from dotenv import load_dotenv
-load_dotenv()
 MONGO_URI = os.environ.get("MONGO_URL") or os.environ.get("MONGODB_URI")
 if not MONGO_URI:
     MONGO_URI = "mongodb://localhost:27017/ethara"
 client = MongoClient(MONGO_URI)
-db = client.get_database()
+db = client.get_database("railway")
 print("Successfully connected to MongoDB!")
