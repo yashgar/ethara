abb57613bf1a2df6a1753a4ae625f6fdeafe3298
Yash Agarwal <117198025+yashgar@users.noreply.github.com>
2026-05-02 00:32:38 +0530

    Update app to use environment variable for port

--- backend/app.py
+++ backend/app.py
@@ -5,6 +5,7 @@ from services.skill_match import get_best_users_for_task
 from routes.auth_routes import auth_bp
 from routes.task_routes import task_bp
 from routes.project_routes import project_bp
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -18,4 +19,5 @@ def server_status():
     return jsonify({"status": "success", "message": "Ethara Backend is running!"}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
