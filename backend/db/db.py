016333831359e265ec902111af89d67b84afdb47
Yash Agarwal <117198025+yashgar@users.noreply.github.com>
2026-05-02 03:48:18 +0530

    Update db.py

--- backend/db/db.py
+++ backend/db/db.py
@@ -1,10 +1,14 @@
-from pymongo import MongoClient
 import os
-MONGO_URI = os.environ.get("MONGO_URL") or os.environ.get("MONGODB_URI")
+from pymongo import MongoClient
+
+MONGO_URI = os.environ.get("MONGO_URL")
+
 if not MONGO_URI:
     MONGO_URI = "mongodb://localhost:27017/ethara"
-safe_url = MONGO_URI.split('@')[-1] if '@' in MONGO_URI else MONGO_URI
-print(f"Attempting to connect to host: {safe_url}")
-client = MongoClient(MONGO_URI,authSource="Admin")
-db = client.get_database("railway")
+
+print(f"⚠️ DEBUG: Booting up with MONGO_URI string length: {len(MONGO_URI)}")
+
+client = MongoClient(MONGO_URI)
+
+db = client.get_database("ethara_db")
 print("Successfully connected to MongoDB!")
